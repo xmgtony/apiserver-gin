9cd82bdd76630c5304be83fd628f93a497e74ea7
maxiaofeng <xmgtony@gmail.com>
2019-12-14 13:21:50 +0800

    [Feat]添加日志和响应结构体

---
+++ common/response/response.go
@@ -0,0 +1,32 @@
+package response
+
+import (
+	error2 "apidemo-gin/common/error"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+// ApiResponse 代表一个响应给客户端的消息结构，包括错误码，错误消息，响应数据
+type ApiResponse struct {
+	ErrCode int         `json:"err_code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
+//SendJson 发送json格式的数据
+func SendJson(c *gin.Context, err error, data interface{}) {
+	code, message := error2.DecodeBizErr(err)
+	// 如果code != 0, 失败的话 返回http状态码400（一般也可以全部返回200）
+	//返回400 更严谨一些，个人接触的项目中大部分都是400。
+	var httpStatus int
+	if code != error2.SUCCESS.ErrCode {
+		httpStatus = http.StatusBadRequest
+	} else {
+		httpStatus = http.StatusOK
+	}
+	c.JSON(httpStatus, ApiResponse{
+		ErrCode: code,
+		Message: message,
+		Data:    data,
+	})
+}
