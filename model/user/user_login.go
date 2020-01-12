94ce2d27d08736601ca7f8acc2d1ece75b93f5ad
maxiaofeng <xmgtony@gmail.com>
2020-01-12 12:12:07 +0800

    [Feat]修复用户名json提交和form提交不一致问题

--- model/user/user_login.go
+++ model/user/user_login.go
@@ -2,6 +2,6 @@ package user
 
 // UserLogin 用户登录
 type UserLogin struct {
-	Name     string `form:"username" json:"name" binding:"required"`
+	Name     string `form:"username" json:"username" binding:"required"`
 	Password string `form:"password" json:"password" binding:"required"`
 }
